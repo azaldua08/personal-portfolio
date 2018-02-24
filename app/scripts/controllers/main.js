b4aba35399db3dd9ff97503e85ed495761c9c2d9
azaldua08 <amiel_zaldua@yahoo.com>
2018-02-25 07:08:59 +0800

    Initial Commit
    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+
+/**
+ * @ngdoc function
+ * @name personalPortfolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personalPortfolioApp
+ */
+angular.module('personalPortfolioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
